3b094bf99bb6d4df8057582d66f15e85e8a2d48c
root <azouine@gmail.com>
2024-04-02 18:48:40 -0700

    Done

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], end=""):
+    i = 0
+    while i < x:
+        try:
+            print("{}".my_list=[], end="")
+            i++
+        except IndexError:
+            break
+        print("")
+        return i
