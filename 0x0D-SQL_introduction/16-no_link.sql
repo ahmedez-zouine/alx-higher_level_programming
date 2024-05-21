59fa9e965e1fc16779198c2c756e418c417f5f9f
root <azouine@gmail.com>
2024-05-21 16:54:47 -0700

    done
    Bonus

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,5 @@
--- Removes all records with a score <= 5 in the table second_table in my MySQL server.
-DELETE FROM `second_table`
-WHERE `score` <= 5;
+-- Displays the average temperature (in Fahrenheit) by city ordered by descending temperature.
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+GROUP BY `city`
+ORDER BY `avg_temp` DESC;
--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,2 @@
--- This Script i will use form print infos about table
-DESCRIBE first_table
-
+-- Prints the full description of the table first_table in my MySQL server.
+SHOW CREATE TABLE `first_table`;
