2380100e726523a9a97c7b2458fcb22907de7231
root <azouine@gmail.com>
2024-05-21 15:06:42 -0700

    Done

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,2 @@
+-- in this Script i will create new tables
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(255));
--- 0x0D-SQL_introduction/4-first_tables.sql
+++
@@ -1,2 +0,0 @@
--- in this Script i will create new tables
-CREATE TABLE IF NOT EXISTS first_table (id int, name char(256));
